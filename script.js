8fe49b3a35f1e7192460eee9f2cef41ffa1aaae4
Nukie <oskarlas.ol@gmail.com>
2022-04-12 19:57:58 +0200

    refactored most of the code

--- script.js
+++ script.js
@@ -15,17 +15,23 @@ let count = 0
 let highestCombo = 0
 let winsAmount = 10
 let duringChoice = false
+let r1
+let r2
 const winChance = .33
 const loseChance = .66
 const tieChance = 1
+const winText = "You Win!"
+const loseText = "You Lose!"
+const tieText = "You Tied!"
 
 winsAmountText.innerText = winsAmount
 // handling restart button
 restartButton.addEventListener("click", ()=>{
         pScore = cScore = highestCombo = count = 0
+        r1 = r2 = null
         images[0].src = "#"
         images[1].src = "#"
-        winner.innerHTML = null
+        winner.innerText = null
         losButton.innerText = "Start"
         combo.innerText = null
         updateScore()
@@ -44,8 +50,6 @@ function startClick() {
 function los() {
     if (!duringChoice) {
         duringChoice = true
-        let r1
-        let r2
 
         async function userChoice() {
             images[0].src = "#"
@@ -61,212 +65,174 @@ function los() {
         function userChoiceKeyPress() {
             return new Promise((resolve) => {
                 document.addEventListener('keydown', onKeyHandler)
-                    function onKeyHandler(e) {
-                        if (e.key === "r") {
-                            r1 = 1
-                            imageText[0].innerText = null
-                            imageText[1].innerText = null
-                            images[0].src = imgtab[r1]
-                            startClick()
-                            document.removeEventListener('keydown', onKeyHandler)
-                            resolve()
-                        }else if (e.key === "p") {
-                            r1 = 2
-                            imageText[0].innerText = null
-                            imageText[1].innerText = null
-                            images[0].src = imgtab[r1]
-                            startClick()
-                            document.removeEventListener('keydown', onKeyHandler)
-                            resolve()
-                        }else if (e.key === "s") {
-                            r1 = 0
-                            imageText[0].innerText = null
-                            imageText[1].innerText = null
-                            images[0].src = imgtab[r1]
-                            startClick()
-                            document.removeEventListener('keydown', onKeyHandler)
-                            resolve()
+                function onKeyHandler(e) {
+                    if (e.key === "r") {
+                        r1 = 1
+                        imageText[0].innerText = null
+                        imageText[1].innerText = null
+                        images[0].src = imgtab[r1]
+                        startClick()
+                        document.removeEventListener('keydown', onKeyHandler)
+                        resolve()
+                    }else if (e.key === "p") {
+                        r1 = 2
+                        imageText[0].innerText = null
+                        imageText[1].innerText = null
+                        images[0].src = imgtab[r1]
+                        startClick()
+                        document.removeEventListener('keydown', onKeyHandler)
+                        resolve()
+                    }else if (e.key === "s") {
+                        r1 = 0
+                        imageText[0].innerText = null
+                        imageText[1].innerText = null
+                        images[0].src = imgtab[r1]
+                        startClick()
+                        document.removeEventListener('keydown', onKeyHandler)
+                        resolve()
+                    }
+
+                    // listen only for needed keys during await
+                    if (e.key === "r" || e.key === "p" || e.key === "s") {
+                        
+                        // winning losing and tying conditions
+                        let randNum = Math.random()
+                        
+                        if (r1==0) {
+                            if (randNum < winChance) {
+                                r2 = 2
+                                images[1].src = imgtab[r2]
+                            }else if (randNum < loseChance) {
+                                r2 = 1
+                                images[1].src = imgtab[r2]
+                            }else if(randNum < tieChance){
+                                r2 = 0
+                                images[1].src = imgtab[r2]
+                            }
+                        }else if(r1==1){
+                            if (randNum < winChance) {
+                                r2 = 0
+                                images[1].src = imgtab[r2]
+                            }else if (randNum < loseChance) {
+                                r2 = 2
+                                images[1].src = imgtab[r2]
+                            }else if(randNum < tieChance){
+                                r2 = 1
+                                images[1].src = imgtab[r2]
+                            }
+                        }else if(r1==2){
+                            if (randNum < winChance) {
+                                r2 = 1
+                                images[1].src = imgtab[r2]
+                            }else if (randNum < loseChance) {
+                                r2 = 0
+                                images[1].src = imgtab[r2]
+                            }else if(randNum < tieChance){
+                                r2 = 2
+                                images[1].src = imgtab[r2]
+                            }
                         }
 
-                        // listen only for needed keys during await
-                        if (e.key === "r" || e.key === "p" || e.key === "s") {
-                            
-                            // winning losing and tying conditions
-                            let randNum = Math.random()
-                            
-                            if (r1==0) {
-                                if (randNum < winChance) {
-                                    r2 = 2
-                                    images[1].src = imgtab[r2]
-                                }else if (randNum < loseChance) {
-                                    r2 = 1
-                                    images[1].src = imgtab[r2]
-                                }else if(randNum < tieChance){
-                                    r2 = 0
-                                    images[1].src = imgtab[r2]
-                                }
-                            }else if(r1==1){
-                                if (randNum < winChance) {
-                                    r2 = 0
-                                    images[1].src = imgtab[r2]
-                                }else if (randNum < loseChance) {
-                                    r2 = 2
-                                    images[1].src = imgtab[r2]
-                                }else if(randNum < tieChance){
-                                    r2 = 1
-                                    images[1].src = imgtab[r2]
-                                }
-                            }else if(r1==2){
-                                if (randNum < winChance) {
-                                    r2 = 1
-                                    images[1].src = imgtab[r2]
-                                }else if (randNum < loseChance) {
-                                    r2 = 0
-                                    images[1].src = imgtab[r2]
-                                }else if(randNum < tieChance){
-                                    r2 = 2
-                                    images[1].src = imgtab[r2]
-                                }
+                        //winner check
+
+                        if (r1 === r2) {
+                            winner.innerText = tieText
+                            count = 0
+                            if (highestCombo<count) {
+                                highestCombo = count
                             }
+                            lastMove = tieText
+                            duringChoice = false
 
-                            //winner check
-
-                            if (r1 === r2) {
-                                winner.innerHTML = "Tie"
-                                if (lastMove == "Tie") {
-                                    if (highestCombo<count) {
-                                        highestCombo = count
-                                    }
+                            return
+                        }
+                        //rock
+                        if (r1 === 1) {
+                            if (r2 === 0) {
+                                winner.innerText = winText
+                                if (lastMove == winText) {
+                                    count++
                                 }else{
-                                    if (highestCombo<count) {
-                                        highestCombo = count
-                                    }
                                     count = 0
                                 }
-                                lastMove = "Tie"
-                                duringChoice = false
-
-                                return
-                            }
-                            //rock
-                            if (r1 === 1) {
-                                if (r2 === 0) {
-                                    winner.innerHTML = "You Win"
-                                    if (lastMove == "You Win") {
-                                        count++
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                    }else{
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                        count = 0
-                                    }
-                                    lastMove = "You Win"
-                                    pScore++
-                                } else{
-                                    winner.innerHTML = "You Loose"
-                                    if (lastMove == "You Loose") {
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                    }else{
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                        count = 0
-                                    }
-                                    lastMove = "You Loose"
-                                    cScore++
+                                if (highestCombo<count) {
+                                    highestCombo = count
                                 }
-                            }
-                            //paper
-                            else if (r1 === 2) {
-                                if (r2 === 1) {
-                                    winner.innerHTML = "You Win"
-                                    if (lastMove == "You Win") {
-                                        count++
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                    }else{
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                        count = 0
-                                    }
-                                    lastMove = "You Win"
-                                    pScore++
-                                } else{
-                                    winner.innerHTML = "You Loose"
-                                    if (lastMove == "You Loose") {
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                    }else{
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                        count = 0
-                                    }
-                                    lastMove = "You Loose"
-                                    cScore++
+                                lastMove = winText
+                                pScore++
+                            } else{
+                                winner.innerText = loseText
+                                count = 0
+                                if (highestCombo<count) {
+                                    highestCombo = count
                                 }
+                                lastMove = loseText
+                                cScore++
                             }
-                            //scissors
-                            else if (r1 === 0) {
-                                if (r2 === 2) {
-                                    winner.innerHTML = "You Win"
-                                    if (lastMove == "You Win") {
-                                        count++
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                    }else{
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                        count = 0
-                                    }
-                                    lastMove = "You Win"
-                                    pScore++
-                                } else{
-                                    winner.innerHTML = "You Loose"
-                                    if (lastMove == "You Loose") {
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                    }else{
-                                        if (highestCombo<count) {
-                                            highestCombo = count
-                                        }
-                                        count = 0
-                                    }
-                                    lastMove = "You Loose"
-                                    cScore++
-                                }
-                            }
-                            updateScore()
-                            if (pScore === winsAmount || cScore === winsAmount) {
-                                restart.classList.add("delay")
-                                restart.classList.remove("unshow")
-                                restart.classList.add("show")
-                                const winner = pScore === winsAmount ? "You Win!" : "You Lost! Try again!"
-                                winnerText.innerText = winner
-                                highestComboText.innerText = highestCombo
-                            }
-
-                            duringChoice = false
                         }
-
-                                    }
-                            })
+                        //paper
+                        else if (r1 === 2) {
+                            if (r2 === 1) {
+                                winner.innerText = winText
+                                if (lastMove == winText) {
+                                    count++
+                                }else{
+                                    count = 0
+                                }
+                                if (highestCombo<count) {
+                                    highestCombo = count
+                                }
+                                lastMove = winText
+                                pScore++
+                            } else{
+                                winner.innerText = loseText
+                                count = 0
+                                if (highestCombo<count) {
+                                    highestCombo = count
+                                }
+                                lastMove = loseText
+                                cScore++
+                            }
                         }
+                        //scissors
+                        else if (r1 === 0) {
+                            if (r2 === 2) {
+                                winner.innerText = winText
+                                if (lastMove == winText) {
+                                    count++
+                                }else{
+                                    count = 0
+                                }
+                                if (highestCombo<count) {
+                                    highestCombo = count
+                                }
+                                lastMove = winText
+                                pScore++
+                            } else{
+                                winner.innerText = loseText
+                                count = 0
+                                if (highestCombo<count) {
+                                    highestCombo = count
+                                }
+                                lastMove = loseText
+                                cScore++
+                            }
+                        }
+                        updateScore()
+                        if (pScore === winsAmount || cScore === winsAmount) {
+                            restart.classList.add("delay")
+                            restart.classList.remove("unshow")
+                            restart.classList.add("show")
+                            const winner = pScore === winsAmount ? winText : loseText + " Try again!"
+                            winnerText.innerText = winner
+                            highestComboText.innerText = highestCombo
+                        }
+                        duringChoice = false
+                    }
+                }
+            })
         }
-
-// fix this braces mess ^^^^^^^^^^^^^^^^
-
+    }
 }
 
 
