50320566a4fe9ba52786ab93d81b492b7e18af5d
Nukie <oskarlas.ol@gmail.com>
2022-04-18 03:40:32 +0200

    Add keybinds to multiplayer (WIP)

--- multiPlayer/script.js
+++ multiPlayer/script.js
@@ -15,12 +15,30 @@ let imageText = document.querySelectorAll(".imageText")
 let settingsIcon = document.querySelector(".settingsIcon")
 let settingsDiv = document.querySelector(".settings")
 let applySettingsButton = document.querySelector(".applySettings")
+let keyBindsButton = document.querySelector(".keyBinds")
+let keyBindsEditorMenu = document.querySelector(".keyBindsEditorMenu")
+let applyKeyBindSettings = document.querySelector(".applyKeyBindSettings")
+let keyBindInputsLabels =  document.querySelectorAll(".keyBindInputsLabels")
+let p1Rock = document.getElementById("p1Rock")
+let p2Rock = document.getElementById("p2Rock")
+let p1Paper = document.getElementById("p1Paper")
+let p2Paper = document.getElementById("p2Paper")
+let p1Scissors = document.getElementById("p1Scissors")
+let p2Scissors = document.getElementById("p2Scissors")
 let emulateButtons = document.querySelector(".emulateButtons")
 let buttonR = document.querySelector(".emulateR")
 let buttonP = document.querySelector(".emulateP")
 let buttonS = document.querySelector(".emulateS")
 let backButton = document.querySelector(".backButton")
 let emulateButtonsCheckBox = document.querySelector(".emulateButtonsCheckBox")
+let keyBinds = {
+    p1Rock: "r",
+    p2Rock: "r",
+    p1Paper: "p",
+    p2Paper: "p",
+    p1Scissors: "s",
+    p2Scissors: "s"
+}
 let p1Score = 0
 let p2Score = 0
 let lastMove
@@ -29,17 +47,23 @@ let count2 = 0
 let highestCombo = 0
 let highestCombo2 = 0
 let winsAmount = 10
+let p1Turn = true
 let duringChoice = false
 let duringGame = false
 let mouseOnBackButton = false
 let shouldEmulateButtons = false
+let regExTest = false
 let r1
 let r2
 const winText = "Player 1 Wins!"
 const winText2 = "Player 2 Wins!"
 const tieText = "Tie!"
 
+window.onload = document.body.classList.remove("preloader")
+
 winsAmountText.innerText = winsAmountUser.value = winsAmount
+
+losButton.addEventListener("click", los)
 
 function mobileCheck() {
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) shouldEmulateButtons = true;})(navigator.userAgent||navigator.vendor||window.opera);
@@ -75,6 +99,52 @@ applySettingsButton.addEventListener("click", ()=>{
     }
 })
 
+keyBindInputsLabels.forEach((element, key)=>{
+    element.value = Object.values(keyBinds)[key].toUpperCase()
+    element.addEventListener("keydown", e=>{
+        if (e.key == "Backspace") {
+            e.preventDefault()
+        }
+        const regex = new RegExp(/^[a-z0-9'=,-./;=[\\\]`]+$/)
+        if (regex.test(e.key)) {
+            regExTest = true
+            keyBindInputsLabels[key].value = e.key.toUpperCase()
+        }
+        else{
+            regExTest = false
+        }
+        if (keyBindInputsLabels[key].value.length > 1) keyBindInputsLabels[key].value = keyBindInputsLabels[key].value.slice(0, 1)
+    })
+})
+
+keyBindsButton.addEventListener("click", ()=>{
+    keyBindsEditorMenu.showModal()
+})
+
+
+applyKeyBindSettings.addEventListener("click", ()=>{
+    if (!duringGame && regExTest && !isNotEmpty()) {
+        keyBinds.p1Rock = p1Rock.value.toLowerCase()
+        keyBinds.p2Rock = p2Rock.value.toLowerCase()
+        keyBinds.p1Paper = p1Paper.value.toLowerCase()
+        keyBinds.p2Paper = p2Paper.value.toLowerCase()
+        keyBinds.p1Scissors = p1Scissors.value.toLowerCase()
+        keyBinds.p2Scissors = p2Scissors.value.toLowerCase()
+        keyBindsEditorMenu.setAttribute("closing", "")
+        keyBindsEditorMenu.addEventListener("animationend", ()=>{
+            keyBindsEditorMenu.removeAttribute("closing")
+            keyBindsEditorMenu.close()
+        }, {once: true} )
+    }
+})
+
+function isNotEmpty() {
+    keyBindInputsLabels.forEach((element, key) => {
+       if (element.value == null || element.value == "") {
+           return false
+       }
+    });
+}
 
 winsAmountUser.addEventListener("input", ()=>{
     if (winsAmountUser.value.length > 4) winsAmountUser.value = winsAmountUser.value.slice(0, 4)
@@ -88,47 +158,47 @@ backButton.addEventListener("mouseover", ()=>{
 backButton.addEventListener("mouseout", ()=>{
     mouseOnBackButton = false
 })
-backButton.addEventListener("animationiteration", handleBackButtonAnimation)
-
-function handleBackButtonAnimation() {
+backButton.addEventListener("animationiteration", ()=>{
     if (!mouseOnBackButton) {
         backButton.classList.remove("animate")
     }
-}
-
+})
 
 // handling restart button
 restartButton.addEventListener("click", ()=>{
-        p1Score = p2Score = highestCombo = highestCombo2 = count = count2 = 0
-        duringGame = false
-        r1 = r2 = lastMove = null
-        images[0].src = "#"
-        images[1].src = "#"
-        winner.innerText = combo.innerText = combo2.innerText = null
-        losButton.innerText = "Start"
-        updateScore()
+    p1Score = p2Score = highestCombo = highestCombo2 = count = count2 = 0
+    duringGame = false
+    r1 = r2 = lastMove = null
+    images[0].src = "#"
+    images[1].src = "#"
+    winner.innerText = combo.innerText = combo2.innerText = null
+    losButton.innerText = "Start"
+    updateScore()
         setTimeout(() => {
             restart.classList.remove("delay")
             
         }, 1000)
         restart.classList.remove("show")
         restart.classList.add("hide")
-})
-
-// handle emulated buttons
-buttonR.addEventListener("click", ()=>{
-    document.dispatchEvent(new KeyboardEvent("keydown", {"key":"r"}))
-})
-
-buttonP.addEventListener("click", ()=>{
-    document.dispatchEvent(new KeyboardEvent("keydown", {"key":"p"}))
-})
-
-buttonS.addEventListener("click", ()=>{
-    document.dispatchEvent(new KeyboardEvent("keydown", {"key":"s"}))
-})
-
-
+    })
+    
+    // handle emulated buttons
+    buttonR.addEventListener("click", ()=>{
+        const key = p1Turn ? keyBinds.p1Rock : keyBinds.p2Rock
+        document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
+    })
+    
+    buttonP.addEventListener("click", ()=>{
+        const key = p1Turn ? keyBinds.p1Paper : keyBinds.p2Paper
+        document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
+    })
+    
+    buttonS.addEventListener("click", ()=>{
+        const key = p1Turn ? keyBinds.p1Scissors : keyBinds.p2Scissors
+        document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
+    })
+    
+    
 function startClick() {
     losButton.innerText = "Next move"
 }
@@ -254,7 +324,7 @@ function winnerCheck() {
 }
 
 // main
-function los() {
+function los(e) {
     settingsDiv.classList.remove("show")
     duringGame = true
     if (!duringChoice) {
@@ -266,9 +336,11 @@ function los() {
             imageText[0].innerText = "Awaitng for user input \navailable options:\nrock = r\n paper = p\n scissors = s"
             imageText[1].innerText = "Waiting for player 1"
             losButton.innerText = "..."
+            p1Turn = p1Turn
             await player1()
             imageText[0].innerText = "User has selected an option"
             imageText[1].innerText = "Awaitng for user input \navailable options:\nrock = r\n paper = p\n scissors = s"
+            p1Turn = !p1Turn
             await player2()
             imageText[0].innerText = null
             imageText[1].innerText = null
@@ -284,15 +356,15 @@ function los() {
             return new Promise((resolve) => {
                 document.addEventListener('keydown', onKeyHandler)
                 function onKeyHandler(e) {
-                    if (e.key === "r") {
+                    if (e.key === keyBinds.p1Rock) {
                         r1 = 1
                         document.removeEventListener('keydown', onKeyHandler)
                         resolve()
-                    }else if (e.key === "p") {
+                    }else if (e.key === keyBinds.p1Paper) {
                         r1 = 2
                         document.removeEventListener('keydown', onKeyHandler)
                         resolve()
-                    }else if (e.key === "s") {
+                    }else if (e.key === keyBinds.p1Scissors) {
                         r1 = 0
                         document.removeEventListener('keydown', onKeyHandler)
                         resolve()
@@ -305,15 +377,15 @@ function los() {
             return new Promise((resolve) => {
                 document.addEventListener('keydown', onKeyHandler)
                 function onKeyHandler(e) {
-                    if (e.key === "r") {
+                    if (e.key === keyBinds.p2Rock) {
                         r2 = 1
                         document.removeEventListener('keydown', onKeyHandler)
                         resolve()
-                    }else if (e.key === "p") {
+                    }else if (e.key === keyBinds.p2Paper) {
                         r2 = 2
                         document.removeEventListener('keydown', onKeyHandler)
                         resolve()
-                    }else if (e.key === "s") {
+                    }else if (e.key === keyBinds.p2Scissors) {
                         r2 = 0
                         document.removeEventListener('keydown', onKeyHandler)
                         resolve()
