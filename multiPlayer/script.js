528113efcc8029d4937f1fbe0c2995e292e2e778
Nukie <oskarlas.ol@gmail.com>
2022-04-20 21:46:44 +0200

    Rewritten duplicate check for keybinds

--- multiPlayer/script.js
+++ multiPlayer/script.js
@@ -54,7 +54,7 @@ let duringGame = false
 let mouseOnBackButton = false
 let shouldEmulateButtons = false
 let isEmulatingButtons = false
-let regExTest = false
+let regExTest = true
 let duplicateKeyBinds = false
 let itemIndex
 let r1
@@ -72,7 +72,7 @@ losButton.addEventListener("click", los)
 function mobileCheck() {
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) shouldEmulateButtons = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return shouldEmulateButtons;
-};
+}
 mobileCheck()
 
 if (shouldEmulateButtons) {
@@ -109,7 +109,7 @@ applySettingsButton.addEventListener("click", ()=>{
         winsAmountText.innerText = winsAmount
         setTimeout(() => {
             settingsDiv.classList.remove("show")
-        }, 200);
+        }, 200)
     }
 })
 
@@ -137,7 +137,8 @@ keyBindInputsLabels.forEach((element, key)=>{
             regExTest = false
         }
         if (keyBindInputsLabels[key].value.length > 1) keyBindInputsLabels[key].value = keyBindInputsLabels[key].value.slice(0, 1)
-        checkDuplicates(key)
+        // checkDuplicates(key)
+        checkDuplicatesv2()
     })
     element.addEventListener("change", ()=>{
         const regex = new RegExp(/[A-Za-z0-9'=,-./;=[\\\]`]+$/)
@@ -149,53 +150,92 @@ keyBindInputsLabels.forEach((element, key)=>{
             keyBindInputsLabels[key].value = null
         }
         if (keyBindInputsLabels[key].value.length > 1) keyBindInputsLabels[key].value = keyBindInputsLabels[key].value.slice(0, 1)
-        checkDuplicates(key)
+        // checkDuplicates(key)
+        checkDuplicatesv2()
     })
 })
 
-
-function checkDuplicates(key) {
+function checkDuplicatesv2() {
     let player1Keys = []
     let player2Keys = []
-    let p1inputIndex = key == 0 ? 0 : key == 2 ? 1 : key == 4 ? 2 : null
-    let p2inputIndex = key == 1 ? 0 : key == 3 ? 1 : key == 5 ? 2 : null
-    keyBindInputsLabels[key].classList.contains("player1Input") ? player1CheckKeys() : player2CheckKeys()
+    let p1DuplicatedKeys = []
+    let p2DuplicatedKeys = []
+
+    player1Input.forEach((element, index)=>{
+        duplicateKeyBinds = false
+        player1Input[index].classList.remove("red")
+        player1Keys.push(element.value)
+    })
+    player2Input.forEach((element, index)=>{
+        duplicateKeyBinds = false
+        player2Input[index].classList.remove("red")
+        player2Keys.push(element.value)
+    })
     
-    function player1CheckKeys() {
-        player1Input.forEach((element, index)=>{
-            if (index == p1inputIndex) {player1Keys.push(null); return}
-            player1Keys.push(element.value)
-        })
-        player1Input.forEach(element=>element.classList.remove("red"))
-        if (player1Keys.some((value, index) => {itemIndex = index; return keyBindInputsLabels[key].value.includes(value)})){
-            keyBindInputsLabels[key].classList.add("red")
-            player1Input[itemIndex].classList.add("red")
-            duplicateKeyBinds = true
-        }else{
-            duplicateKeyBinds = false
-            keyBindInputsLabels[key].classList.remove("red")
-            player1Input[itemIndex].classList.remove("red")
-        }
-    }
-    function player2CheckKeys() {
-        player2Input.forEach((element, index)=>{
-            if (index == p2inputIndex) {player2Keys.push(null); return}
-            player2Keys.push(element.value)
-        })
-        player2Input.forEach(element=>element.classList.remove("red"))
-        if (player2Keys.some((value, index) => {itemIndex = index; return keyBindInputsLabels[key].value.includes(value)})){
-            duplicateKeyBinds = true
-            keyBindInputsLabels[key].classList.add("red")
-            player2Input[itemIndex].classList.add("red")
-        }else{
-            duplicateKeyBinds = false
-            keyBindInputsLabels[key].classList.remove("red")
-            player2Input[itemIndex].classList.remove("red")
-        }
-    }
-    console.log(player1Keys)
-    console.log(player2Keys)
+    player1Keys.forEach((element, index) => {
+        if (index != player1Keys.indexOf(element)) p1DuplicatedKeys.push(index, player1Keys.indexOf(element))
+    })
+    player2Keys.forEach((element, index) => {
+        if (index != player2Keys.indexOf(element)) p2DuplicatedKeys.push(index, player2Keys.indexOf(element))
+    })
+
+    p1DuplicatedKeys.forEach((element, key)=>{
+        player1Input[element].classList.add("red")
+        duplicateKeyBinds = true
+    })
+    p2DuplicatedKeys.forEach((element, key)=>{
+        player2Input[element].classList.add("red")
+        duplicateKeyBinds = true
+    })
+
+    // console.log(p1DuplicatedKeys);
+    // console.log(p2DuplicatedKeys);
 }
+
+// old duplicate key highlighter
+
+// function checkDuplicates(key) {
+//     let player1Keys = []
+//     let player2Keys = []
+//     let p1inputIndex = key == 0 ? 0 : key == 2 ? 1 : key == 4 ? 2 : null
+//     let p2inputIndex = key == 1 ? 0 : key == 3 ? 1 : key == 5 ? 2 : null
+//     keyBindInputsLabels[key].classList.contains("player1Input") ? player1CheckKeys() : player2CheckKeys()
+    
+//     function player1CheckKeys() {
+//         player1Input.forEach((element, index)=>{
+//             if (index == p1inputIndex) {player1Keys.push(null); return}
+//             player1Keys.push(element.value)
+//         })
+//         player1Input.forEach(element=>element.classList.remove("red"))
+//         if (player1Keys.some((value, index) => {itemIndex = index; return keyBindInputsLabels[key].value.includes(value)})){
+//             keyBindInputsLabels[key].classList.add("red")
+//             player1Input[itemIndex].classList.add("red")
+//             duplicateKeyBinds = true
+//         }else{
+//             duplicateKeyBinds = false
+//             keyBindInputsLabels[key].classList.remove("red")
+//             player1Input[itemIndex].classList.remove("red")
+//         }
+//     }
+//     function player2CheckKeys() {
+//         player2Input.forEach((element, index)=>{
+//             if (index == p2inputIndex) {player2Keys.push(null); return}
+//             player2Keys.push(element.value)
+//         })
+//         player2Input.forEach(element=>element.classList.remove("red"))
+//         if (player2Keys.some((value, index) => {itemIndex = index; return keyBindInputsLabels[key].value.includes(value)})){
+//             duplicateKeyBinds = true
+//             keyBindInputsLabels[key].classList.add("red")
+//             player2Input[itemIndex].classList.add("red")
+//         }else{
+//             duplicateKeyBinds = false
+//             keyBindInputsLabels[key].classList.remove("red")
+//             player2Input[itemIndex].classList.remove("red")
+//         }
+//     }
+//     console.log(player1Keys)
+//     console.log(player2Keys)
+// }
 
 
 keyBindsButton.addEventListener("click", ()=>{
@@ -232,7 +272,7 @@ function isNotEmpty() {
        if (element.value == null || element.value == "") {
            return false
        }
-    });
+    })
 }
 
 winsAmountUser.addEventListener("input", ()=>{
@@ -269,24 +309,24 @@ restartButton.addEventListener("click", ()=>{
         }, 1000)
         restart.classList.remove("show")
         restart.classList.add("hide")
-    })
+})
     
-    // handle emulated buttons
-    buttonR.addEventListener("click", ()=>{
-        const key = p1Turn ? keyBinds.player1.Rock : keyBinds.player2.Rock
-        document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
-    })
-    
-    buttonP.addEventListener("click", ()=>{
-        const key = p1Turn ? keyBinds.player1.Paper : keyBinds.player2.Paper
-        document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
-    })
-    
-    buttonS.addEventListener("click", ()=>{
-        const key = p1Turn ? keyBinds.player1.Scissors : keyBinds.player2.Scissors
-        document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
-    })
-    
+// handle emulated buttons
+buttonR.addEventListener("click", ()=>{
+    const key = p1Turn ? keyBinds.player1.Rock : keyBinds.player2.Rock
+    document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
+})
+
+buttonP.addEventListener("click", ()=>{
+    const key = p1Turn ? keyBinds.player1.Paper : keyBinds.player2.Paper
+    document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
+})
+
+buttonS.addEventListener("click", ()=>{
+    const key = p1Turn ? keyBinds.player1.Scissors : keyBinds.player2.Scissors
+    document.dispatchEvent(new KeyboardEvent("keydown", {"key":key}))
+})
+
     
 function startClick() {
     losButton.innerText = "Next move"
@@ -425,11 +465,11 @@ function los(e) {
             imageText[0].innerText = `Awaitng for user input \navailable options:\nrock = ${keyBinds.player1.Rock}\n paper = ${keyBinds.player1.Paper}\n scissors = ${keyBinds.player1.Scissors}`
             imageText[1].innerText = "Waiting for player 1"
             losButton.innerText = "..."
-            p1Turn = p1Turn
+            p1Turn = true
             await player1()
             imageText[0].innerText = "User has selected an option"
             imageText[1].innerText = `Awaitng for user input \navailable options:\nrock = ${keyBinds.player2.Rock}\n paper = ${keyBinds.player2.Paper}\n scissors = ${keyBinds.player2.Scissors}`
-            p1Turn = !p1Turn
+            p1Turn = false
             await player2()
             imageText[0].innerText = null
             imageText[1].innerText = null
