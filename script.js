9ae0c1214e28a3619381a1cc8a4fc2243165f26f
Nukie <oskarlas.ol@gmail.com>
2022-05-04 23:43:34 +0200

    Feat: link to something....
    Co-authored-by: Radziu47 <Radziu47@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ if (sessionStorage.getItem("secret")) {
     const Radziu = document.createElement("a")
     const p = document.createElement("p")
     p.innerText = sessionStorage.getItem("secret")
-    Radziu.href = "#"
+    Radziu.href = "https://radziu47.github.io/Asperia/"
     Radziu.appendChild(p)
     Radziu.className = "Radziu"
     document.querySelector(".main").appendChild(Radziu)
