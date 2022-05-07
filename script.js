3d86d0065e5ed5c1115f39525390d3cb2de09c0d
Nukie <oskarlas.ol@gmail.com>
2022-05-07 02:26:58 +0200

    Feat: easteregg collab
    Co-authored-by: Radziu47 <Radziu47@users.noreply.github.com>

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ if (sessionStorage.getItem("secret")) {
     Radziu.href = "https://radziu47.github.io/Asperia/"
     Radziu.appendChild(p)
     Radziu.className = "Radziu"
+    Radziu.addEventListener("click", e => {
+        sessionStorage.setItem("eventTriggered", true)
+    })
     document.querySelector(".main").appendChild(Radziu)
+}
 
+if (sessionStorage.getItem("code1")) {
+    document.querySelector(".overlay").classList.add("show")
+    document.title = 4829
+    sessionStorage.removeItem("code1")
+}
+if (sessionStorage.getItem("code2")) {
+    document.querySelector(".overlay").classList.add("show")
+    document.title = 8207
+    sessionStorage.removeItem("code2")
 }
--- singlePlayer/script.js
+++ singlePlayer/script.js
@@ -35,7 +35,7 @@ let duringChoice = false
 let duringGame = false
 let mouseOnBackButton = false
 let shouldEmulateButtons = false
-// let chancesPercentOutputBelow = false
+let event = false
 let r1
 let r2
 let winChance = 33
@@ -44,9 +44,35 @@ let tieChance = 99
 const winText = "You Win!"
 const loseText = "You Lose!"
 const tieText = "You Tied!"
+let eventOptions = []
+let eventCombinationsCounter = 0
+let firstCodeTriggered = false
+const code1 = [
+    [1, 1],
+    [2, 1],
+    [0, 0]
+]
+const code2 = [
+    [2, 0],
+    [1, 1],
+    [0, 1]
+]
+
+if (sessionStorage.getItem("eventTriggered")) {
+    event = true
+    const overlay = document.querySelector(".overlay")
+    const eventDem = document.getElementById("eventDem")
+    const comboDiv = document.querySelector(".combo")
+    overlay.classList.add("show")
+    eventDem.textContent = "Demon"
+    winsAmount = 3
+    settingsDiv.remove()
+    settingsIcon.remove()
+    comboDiv.remove()
+    restart.remove()
+}
 
 winsAmountText.innerText = winsAmountUser.value = winsAmount
-// chancesPercentOutput.innerText = (winChance + 1) + (loseChance - winChance) + (tieChance - loseChance)
 
 function mobileCheck() {
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) shouldEmulateButtons = true;})(navigator.userAgent||navigator.vendor||window.opera);
@@ -114,7 +140,6 @@ winChanceUser.addEventListener("input", ()=>{
         tieChanceUser.value = 0
         loseChanceUser.value = 100 - (parseInt(winChanceUser.value) + parseInt(tieChanceUser.value))
     }
-    // chancesPercentOutput.innerText = parseInt(winChanceUser.value) + parseInt(loseChanceUser.value) + parseInt(tieChanceUser.value)
 })
 loseChanceUser.addEventListener("input", ()=>{
     if (loseChanceUser.value > 100) loseChanceUser.value = 100
@@ -124,8 +149,6 @@ loseChanceUser.addEventListener("input", ()=>{
         tieChanceUser.value = 0
         winChanceUser.value = 100 - (parseInt(loseChanceUser.value) + parseInt(tieChanceUser.value))
     }
-    // chancesPercentOutput.innerText = parseInt(winChanceUser.value) + parseInt(loseChanceUser.value) + parseInt(tieChanceUser.value)
-    
 })
 backButton.addEventListener("mouseover", ()=>{
     backButton.classList.add("animate")
@@ -202,6 +225,7 @@ function los() {
                 function onKeyHandler(e) {
                     if (e.key === "r") {
                         r1 = 1
+                        eventOptions.push(r1)
                         imageText[0].innerText = null
                         imageText[1].innerText = null
                         images[0].src = imgtab[r1]
@@ -210,6 +234,7 @@ function los() {
                         resolve()
                     }else if (e.key === "p") {
                         r1 = 2
+                        eventOptions.push(r1)
                         imageText[0].innerText = null
                         imageText[1].innerText = null
                         images[0].src = imgtab[r1]
@@ -218,6 +243,7 @@ function los() {
                         resolve()
                     }else if (e.key === "s") {
                         r1 = 0
+                        eventOptions.push(r1)
                         imageText[0].innerText = null
                         imageText[1].innerText = null
                         images[0].src = imgtab[r1]
@@ -241,40 +267,67 @@ function los() {
                     if (e.key === "r" || e.key === "p" || e.key === "s") {
                         
                         // winning losing and tying conditions
-                        let randNum = Math.floor(Math.random() * 100)
+                        let randNum = Math.floor(Math.random() * 99)
                         
                         if (r1==0) {
                             if (randNum < winChance) {
                                 r2 = 2
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }else if (randNum < loseChance) {
                                 r2 = 1
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }else if (randNum < tieChance){
                                 r2 = 0
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }
                         }else if(r1==1){
                             if (randNum < winChance) {
                                 r2 = 0
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }else if (randNum < loseChance) {
                                 r2 = 2
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }else if (randNum < tieChance){
                                 r2 = 1
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }
                         }else if(r1==2){
                             if (randNum < winChance) {
                                 r2 = 1
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }else if (randNum < loseChance) {
                                 r2 = 0
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
                             }else if (randNum < tieChance){
                                 r2 = 2
+                                eventOptions.push(r2)
                                 images[1].src = imgtab[r2]
+                            }
+                        }
+
+                        if (eventOptions.every((value, index) => value == code1[eventCombinationsCounter][index])) {
+                            firstCodeTriggered = true
+                            eventCombinationsCounter++
+                            eventOptions = []
+                            if (eventCombinationsCounter >= 3) {
+                                sessionStorage.setItem("code1", true)
+                                window.location.href = "../"
+                            }
+                        }
+                        if (eventOptions.every((value, index) => value == code2[eventCombinationsCounter][index]) && !firstCodeTriggered) {
+                            eventCombinationsCounter++
+                            eventOptions = []
+                            if (eventCombinationsCounter >= 3) {
+                                sessionStorage.setItem("code2", true)
+                                window.location.href = "../"
                             }
                         }
 
@@ -366,12 +419,25 @@ function los() {
                         }
                         updateScore()
                         if (pScore === winsAmount || cScore === winsAmount) {
-                            restart.classList.add("delay")
-                            restart.classList.remove("hide")
-                            restart.classList.add("show")
-                            const winner = pScore === winsAmount ? winText : loseText + " Try again!"
-                            winnerText.innerText = winner
-                            highestComboText.innerText = highestCombo
+                            if (event) {
+                                pScore = cScore = highestCombo = count = eventCombinationsCounter = 0
+                                duringGame = false
+                                firstCodeTriggered = false
+                                r1 = r2 = lastMove = null
+                                images[0].src = "#"
+                                images[1].src = "#"
+                                winner.innerText = null
+                                losButton.innerText = "Start"
+                                combo.innerText = null
+                                updateScore()
+                            }else {
+                                restart.classList.add("delay")
+                                restart.classList.remove("hide")
+                                restart.classList.add("show")
+                                const winner = pScore === winsAmount ? winText : loseText + " Try again!"
+                                winnerText.innerText = winner
+                                highestComboText.innerText = highestCombo
+                            }
                         }
                         duringChoice = false
                     }
